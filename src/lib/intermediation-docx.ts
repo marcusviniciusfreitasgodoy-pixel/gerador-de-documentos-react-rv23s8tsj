61ee1c5b920d49386876d8eb2027e7860e98bace
Skip <skip@usecurling.com>
2026-07-16 14:39:08 -0300

    Auto-commit after full sync (build simulation)

--- src/lib/intermediation-docx.ts
+++ src/lib/intermediation-docx.ts
@@ -1,8 +1,8 @@
 import { renderFromUrl, extractTextFromUrl } from '@/lib/docx-generator'
 
 const AUTORIZACAO_TEMPLATE_URL =
-  'https://gist.githubusercontent.com/marcusviniciusfreitasgodoy-pixel/2fc9ab475e6486132bab6a43b8dc1d34/raw/78049bbc3929f9ee63333d86e41e311ce2061c44/autorizacao_base64.txt'
-const AUTORIZACAO_EXPECTED_BYTES = 38486
+  'https://gist.githubusercontent.com/marcusviniciusfreitasgodoy-pixel/2fc9ab475e6486132bab6a43b8dc1d34/raw/10b101d04fec3b0d19c3e04cf819793099cfadc1/autorizacao_base64.txt'
+const AUTORIZACAO_EXPECTED_BYTES = 38689
 
 export async function generateIntermediationDocx(data: Record<string, string>): Promise<void> {
   await renderFromUrl(
