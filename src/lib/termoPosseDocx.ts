7dbbba706b8220d988e27003b877f524f3a3fd22
Skip <skip@usecurling.com>
2026-07-16 14:48:43 -0300

    Auto-commit after full sync (build simulation)

--- src/lib/termoPosseDocx.ts
+++ src/lib/termoPosseDocx.ts
@@ -1,8 +1,8 @@
 import { renderFromUrl, extractTextFromUrl } from '@/lib/docx-generator'
 
 const TERMO_POSSE_TEMPLATE_URL =
-  'https://gist.githubusercontent.com/marcusviniciusfreitasgodoy-pixel/2fc9ab475e6486132bab6a43b8dc1d34/raw/94aabdc515aaa2ad9c775ed6f5db0cbafd82a804/termo_posse_base64.txt'
-const TERMO_POSSE_EXPECTED_BYTES = 38692
+  'https://gist.githubusercontent.com/marcusviniciusfreitasgodoy-pixel/2fc9ab475e6486132bab6a43b8dc1d34/raw/c05a2f9dd3303f8977ab8e7a2a0a6e10a1219b88/termo_posse_base64.txt'
+const TERMO_POSSE_EXPECTED_BYTES = 38892
 
 export async function generateTermoPosseDocx(data: Record<string, string>): Promise<void> {
   await renderFromUrl(
